f2fdc1ed811a1bb2db249f4f0b8325bb4d07d5e4
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2024-09-23 23:09:37 +0530

    Changes for hosting

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from flask_compress import Compress
+from flask_talisman import Talisman
 
 app = Flask(__name__)
+Compress(app)  # Enable compression for responses
+
+Talisman(app)  # Enable security headers
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
